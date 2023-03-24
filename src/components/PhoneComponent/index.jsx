2f9dc55b3a64bf44e450800c00612600f767d32a
TimotheGonin <gonin.timothe@gmail.com>
2023-03-24 15:48:38 +0100

    Connect Map State To Props

--- src/components/PhoneComponent/index.jsx
+++ src/components/PhoneComponent/index.jsx
@@ -1,16 +1,24 @@
 import React from "react";
 import phone from "../../images/phone.png";
-const PhoneComponent = () => {
+import { connect } from "react-redux";
+const PhoneComponent = (props) => {
+	console.log(props.phones);
 	return (
 		<div className="container">
 			<img src={phone} alt="phone" />
 			<p>
-				Disponibilité
-				<span id="count"></span>
+				Disponibilité:
+				<span id="count">{props.phones}</span>
 			</p>
 			<button>Acheter</button>
 		</div>
 	);
 };
 
-export default PhoneComponent;
+const mapStateToProps = (state) => {
+	return {
+		phones: state.phones,
+	};
+};
+
+export default connect(mapStateToProps)(PhoneComponent);
