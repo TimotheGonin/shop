774d019502bcfaf248c3f4d4bbf7c6f0775bf60a
TimotheGonin <gonin.timothe@gmail.com>
2023-03-30 11:11:35 +0200

    add redux-thunk + applyMiddleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,14 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import phoneReducer from "./phone/reducerPhone";
 import tvReducer from "./tv/reducerTv";
 import commentReducer from "./comments/reducerComments";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
 	phone: phoneReducer,
 	television: tvReducer,
 	comments: commentReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
